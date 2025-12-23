1b2cbd9f59e7ac9e4021c593d2932e8a6d27cba7
Virgil I Ambar <imanuelambar11@gmail.com>
2025-12-23 22:43:40 +0700

    fix: uncomment syntax

--- main.go
+++ main.go
@@ -3,30 +3,30 @@ package main
 // minitask 1
 
 import (
-	// "fmt"
+	"fmt"
 
-	// task1 "github.com/virgilIw/koda-b5-go/internals/minitask1"
-	// task2 "github.com/virgilIw/koda-b5-go/internals/minitask2"
-	// task3 "github.com/virgilIw/koda-b5-go/internals/minitask3"
-	// task4 "github.com/virgilIw/koda-b5-go/internals/minitask4"
+	task1 "github.com/virgilIw/koda-b5-go/internals/minitask1"
+	task2 "github.com/virgilIw/koda-b5-go/internals/minitask2"
+	task3 "github.com/virgilIw/koda-b5-go/internals/minitask3"
+	task4 "github.com/virgilIw/koda-b5-go/internals/minitask4"
 	task5 "github.com/virgilIw/koda-b5-go/internals/minitask5"
 )
 
 func main() {
-	// var phi float64
-	// var jari float64
-	// //
-	// phi = 22 / 7
-	// jari = 10
-	// //
-	// limit := 5
-	// //
-	// task1.Minitask1(phi, jari)
-	// fmt.Println("===========")
-	// task2.Minitask2(limit)
-	// fmt.Println("===========")
-	// task3.Minitask3()
-	// fmt.Println("===========")
-	// task4.Minitask4()
+	var phi float64
+	var jari float64
+	//
+	phi = 22 / 7
+	jari = 10
+	//
+	limit := 5
+	//
+	task1.Minitask1(phi, jari)
+	fmt.Println("===========")
+	task2.Minitask2(limit)
+	fmt.Println("===========")
+	task3.Minitask3()
+	fmt.Println("===========")
+	task4.Minitask4()
 	task5.Minitask5()
 }
