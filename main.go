d3b36e302529b96729181d33432bc3952f812879
Virgil I Ambar <imanuelambar11@gmail.com>
2025-12-25 01:41:44 +0700

    fix: make main.go looking goodor clean

--- main.go
+++ main.go
@@ -3,39 +3,39 @@ package main
 // minitask 1
 
 import (
-	// task1 "github.com/virgilIw/koda-b5-go/internals/minitask1"
-	// task2 "github.com/virgilIw/koda-b5-go/internals/minitask2"
-	// task3 "github.com/virgilIw/koda-b5-go/internals/minitask3"
-	// task4 "github.com/virgilIw/koda-b5-go/internals/minitask4"
-	// task5 "github.com/virgilIw/koda-b5-go/internals/minitask5"
-	// task6 "github.com/virgilIw/koda-b5-go/internals/minitask6"
+	"fmt"
 
+	task1 "github.com/virgilIw/koda-b5-go/internals/minitask1"
+	task2 "github.com/virgilIw/koda-b5-go/internals/minitask2"
+	task3 "github.com/virgilIw/koda-b5-go/internals/minitask3"
+	task4 "github.com/virgilIw/koda-b5-go/internals/minitask4"
+	task5 "github.com/virgilIw/koda-b5-go/internals/minitask5"
+	minitask6 "github.com/virgilIw/koda-b5-go/internals/minitask6"
 	task7 "github.com/virgilIw/koda-b5-go/internals/minitask7"
-	// task8 "github.com/virgilIw/koda-b5-go/internals/minitask8"
+	task8 "github.com/virgilIw/koda-b5-go/internals/minitask8"
 )
 
 func main() {
-	// var phi float64
-	// var jari float64
-	// //
-	// phi = 22 / 7
-	// jari = 10
-	// //
-	// limit := 5
-	// //
-	// task1.Minitask1(phi, jari)
-	// fmt.Println("===========")
-	// task2.Minitask2(limit)
-	// fmt.Println("===========")
-	// task3.Minitask3()
-	// fmt.Println("===========")
-	// task4.Minitask4()
-	// task5.Minitask5()
-	// task6.Minitask6()
-	// task6.Testing()
-	// fmt.Println("\n===========")
-	// fmt.Println("\n===========")
-	//
+	phi := 22.0 / 7.0
+	jari := 10.0
+	limit := 5
+
+	task1.Minitask1(phi, jari)
+	fmt.Println("===========")
+
+	task2.Minitask2(limit)
+	fmt.Println("===========")
+
+	task3.Minitask3()
+	fmt.Println("===========")
+
+	task4.Minitask4()
+	task5.Minitask5()
+	minitask6.Testing()
+
+	fmt.Println("\n===========")
 	task7.Minitask7()
-	// task8.PaymentMethod()
+
+	fmt.Println("\n===========")
+	task8.PaymentMethod()
 }
